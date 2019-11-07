0f1d33a4d0a8ab3b337885e1c3ec12937a3b99e0
Ngoc Nguyen <ngoc1414@Ukahon.lan>
2019-11-07 17:57:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/roombooking/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.example.roombooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity
+{
+
+}
---
+++ app/src/main/java/com/example/roombooking/Fragments/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.example.roombooking.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment
+{
+
+}
