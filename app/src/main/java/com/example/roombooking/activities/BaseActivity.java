b1b12a40e4495efc96a32c147a663a47ca5b7d8b
dgminhngoc <dgminhngoc@gmail.com>
2019-11-09 20:13:04 +0100

    Correct Base BaseActivity should be an abstract class, in this case it mustn't be registered in Manifest

--- app/src/main/java/com/example/roombooking/activities/BaseActivity.java
+++ app/src/main/java/com/example/roombooking/activities/BaseActivity.java
@@ -6,16 +6,16 @@ import com.example.roombooking.R;
 import com.example.roombooking.controllers.SwitchFragmentController;
 import com.example.roombooking.fragments.BaseFragment;
 
-public class BaseActivity extends AppCompatActivity
+public abstract class BaseActivity extends AppCompatActivity
 {
 	/**
 	 * switch to a fragment,and add it to back stack
 	 *
 	 * @param pBaseChildFragment: the instance of fragment that want to show
 	 */
-	protected <T extends BaseFragment> void switchFragment (T pBaseChildFragment, boolean isAddToBackstack)
+	protected <T extends BaseFragment> void switchFragment (T pBaseChildFragment, boolean isAddToBackStack)
 	{
-		SwitchFragmentController.switchFragment(R.id.containerView, getSupportFragmentManager(), pBaseChildFragment, isAddToBackstack);
+		SwitchFragmentController.switchFragment(R.id.containerView, getSupportFragmentManager(), pBaseChildFragment, isAddToBackStack);
 	}
 
 	@Override
--- app/src/main/java/com/example/roombooking/activities/MainActivity.java
+++ app/src/main/java/com/example/roombooking/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.roombooking.activities;
 import android.os.Bundle;
 
 import com.example.roombooking.R;
-import com.example.roombooking.fragments.TestFragmentA;
 
 public class MainActivity extends BaseActivity
 {
@@ -13,7 +12,5 @@ public class MainActivity extends BaseActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		switchFragment(new TestFragmentA(), true);
 	}
 }
