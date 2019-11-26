60326d69fd0b6e0cbec96a37e0dc6dc70afe45c4
amittel <arnulf.mittelstaedt@gmail.com>
2019-11-26 12:35:40 +0100

    Started Loginscreen

--- app/src/main/java/com/example/roombooking/activities/MainActivity.java
+++ app/src/main/java/com/example/roombooking/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.roombooking.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.roombooking.R;
+import com.example.roombooking.fragments.LoginScreenFragment;
 
 public class MainActivity extends BaseActivity
 {
@@ -12,5 +19,23 @@ public class MainActivity extends BaseActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		View.OnClickListener listener = new View.OnClickListener() {
+			public void onClick(View view) {
+				Fragment fragment = null;
+				if(view == findViewById(R.id.btn_login)){
+					fragment = new LoginScreenFragment();
+				}
+				FragmentManager manager = getSupportFragmentManager();
+				FragmentTransaction transaction = manager.beginTransaction();
+				transaction.replace(R.id.containerView, fragment);
+				transaction.commit();
+			}
+		};
+		Button btn1 = (Button)findViewById(R.id.btn_login);
+		btn1.setOnClickListener(listener);
+
 	}
+
+
 }
