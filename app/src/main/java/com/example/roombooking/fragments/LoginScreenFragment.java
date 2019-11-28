7a96024c84e076fdeb9f58e3e446f1666636e6fd
amittel <arnulf.mittelstaedt@gmail.com>
2019-11-28 13:42:23 +0100

    made button work

--- app/src/main/java/com/example/roombooking/fragments/LoginScreenFragment.java
+++ app/src/main/java/com/example/roombooking/fragments/LoginScreenFragment.java
@@ -1,6 +1,7 @@
 package com.example.roombooking.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import com.example.roombooking.R;
 
 
 
-public class LoginScreenFragment extends BaseFragment implements View.OnClickListener
+public class LoginScreenFragment extends BaseFragment
 {
 
 
@@ -30,13 +31,34 @@ public class LoginScreenFragment extends BaseFragment implements View.OnClickLis
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_login_screen, container, false);
 
-		Button loginBtn = view.findViewById(R.id.btn_login);
-		loginBtn.setOnClickListener(this);
-
-		EditText text = view.findViewById(R.id.txt_User);
-		String user = text.getText().toString();
+		final EditText text = view.findViewById(R.id.txt_User);
+		final String user = text.getText().toString();
 		EditText text_pass = view.findViewById(R.id.txt_Pass);
 		String pass = text_pass.getText().toString();
+		final String u = "laser";
+		Button loginBtn = view.findViewById(R.id.btn_login);
+		loginBtn.setOnClickListener( new View.OnClickListener()
+		{
+			public void onClick(View view)
+			{
+				if(text.getText().toString().equals("Tim")) {
+					Toast.makeText(getContext(), "User "+ text.getText().toString() + " eingeloggt" , Toast.LENGTH_LONG).show();
+
+				}
+				else {
+					Toast.makeText(getContext(), "Failed!", Toast.LENGTH_LONG).show();
+				}
+				Log.v("EditText", text.getText().toString());
+				Log.d("benutzer", user);
+
+			}
+		});
+
+
+
+
+
+
 
 
 
@@ -45,16 +67,4 @@ public class LoginScreenFragment extends BaseFragment implements View.OnClickLis
 	}
 
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()) {
-			case R.id.btn_login:
-				//ToDo: Validate
-
-				Toast.makeText(getContext(), "Einloggen: ", Toast.LENGTH_SHORT).show();
-				break;
-
-		}
-
-	}
 }
